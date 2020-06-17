13e2a9136101d2108774903f9c8be4f33d85096e
BerezinD <ghost_6600@mail.ru>
2020-06-18 00:06:16 +0300

    A little refactoring

--- app/src/main/java/com/tommy/simpleweather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/tommy/simpleweather/ui/home/HomeFragment.kt
@@ -2,24 +2,24 @@ package com.tommy.simpleweather.ui.home
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.tommy.simpleweather.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment: Fragment(R.layout.fragment_home), View.OnClickListener {
+class HomeFragment : Fragment(R.layout.fragment_home), View.OnClickListener {
 
     private lateinit var navController: NavController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        view.findViewById<Button>(R.id.humidity_button).setOnClickListener(this)
+        humidity_button.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when(v!!.id) {
+        when (v!!.id) {
             R.id.humidity_button -> navController.navigate(R.id.action_from_home_to_humidity)
         }
     }
--- app/src/main/java/com/tommy/simpleweather/ui/main/MainActivity.kt
+++ app/src/main/java/com/tommy/simpleweather/ui/main/MainActivity.kt
@@ -7,40 +7,8 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.tommy.simpleweather.R
-import com.tommy.simpleweather.ui.home.HomeFragment
-import com.tommy.simpleweather.ui.statistics.StatisticsFragment
 
 class MainActivity : AppCompatActivity() {
-
-    private var fragmentHome : HomeFragment = HomeFragment()
-    private var fragmentStatistics : StatisticsFragment = StatisticsFragment()
-
-    // TODO: change logic of the fragments replacement
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                val transaction = supportFragmentManager.beginTransaction()
-                transaction.replace(R.id.nav_host_fragment, fragmentHome)
-                transaction.commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_statistics -> {
-                val transaction = supportFragmentManager.beginTransaction()
-                transaction.replace(R.id.nav_host_fragment, fragmentStatistics)
-                transaction.commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_settings -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_refresh -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
